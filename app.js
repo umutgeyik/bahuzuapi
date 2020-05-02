c69ffda00b48102fbac6580e8ced5a1a765ef92b
umutgyk95@gmail.com <ug@Umuts-MacBook-Pro.local>
2020-05-02 16:35:48 +0300

    new proc

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Listening on port 3000")
 })
 
