ec241bff9110dcc8418a4a12d74bf43232140849
umutgyk95@gmail.com <ug@Umuts-MacBook-Pro.local>
2020-05-02 15:52:17 +0300

    selam

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+
+
+app.use(express.json())
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
+
+app.post('/cardDetails',(req,res) => {
+
+    console.log(req.body)
+    const newUser = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        orhunc: req.body.orhunc
+    }
+    console.log(req)
+    console.log(newUser.name)
+    console.log(newUser.orhunc)
+    if(newUser.name == 'Umut'){
+        res.status(200).send()
+    } else {
+        res.status(250).send("SICTIN BASGANIMMM")
+    }
+})
+
+
+app.get('/',(req,res) =>{
+    console.log(req.body)
+    console.log('BOS OLANA GIRDIK')
+    res.send('BOSBOS')
+})
+
+app.get('/posts',(req,res) =>{
+    console.log(req.body)
+    console.log('Dolu olana girdik')
+    res.send('dolu dolu')
+})
