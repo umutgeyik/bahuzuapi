457292c184778fdfd61cc6c97b0be663c76cc781
umutgyk95@gmail.com <ug@Umuts-MacBook-Pro.local>
2020-05-02 18:19:44 +0300

    sdasdds

--- app.js
+++ app.js
@@ -9,18 +9,17 @@ app.listen(process.env.PORT || 5000, () => {
 
 app.get('/cardDetails',(req,res) => {
 
-    console.log(req.body)
-    const newUser = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }
+//    console.log(req.body)
+  //  const newUser = {
+    //    name: req.body.name,
+      //  email: req.body.email,
+       // password: req.body.password,
+    //}
 
     res.status(200).send()
 })
 
 app.get('/',(req,res) =>{
-    console.log(req.body)
     console.log('BOS OLANA GIRDIK')
     res.send('BOSBOS')
 })
@@ -30,7 +29,6 @@ app.get('/posts',(req,res) =>{
     const post = {
         text:"Merhaba Deneniyorsunuz"
     }
-    console.log(req.body)
     console.log('Dolu olana girdik')
     res.status(200).send()
 })
